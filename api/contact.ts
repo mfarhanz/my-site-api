6fbed115107936dc12451a45f8859541338001cb
mfarhan <mfarhan@mun.ca>
2025-10-16 14:43:12 -0400

    use es instead of commmonjs

--- api/contact.ts
+++ api/contact.ts
@@ -2,39 +2,39 @@
 // import { env } from '$env/dynamic/private';
 
 // export const POST = async ({ request }) => {
-  // const { name, email, message } = await request.json();
-  // const mailjet = Mailjet.apiConnect(env.MJ_APIKEY_PUBLIC, env.MJ_APIKEY_PRIVATE);
+// const { name, email, message } = await request.json();
+// const mailjet = Mailjet.apiConnect(env.MJ_APIKEY_PUBLIC, env.MJ_APIKEY_PRIVATE);
 
-  // try {
-    // await mailjet.post('send', { version: 'v3.1' }).request({
-      // Messages: [
-        // {
-          // From: {
-            // Email: 'mfz.bin@gmail.com',
-            // Name: 'Portfolio Contact'
-          // },
-          // To: [
-            // {
-              // Email: 'mfz.bin@gmail.com',
-              // Name: 'You'
-            // }
-          // ],
-          // TemplateID: 7402715,
-          // TemplateLanguage: true,
-          // Variables: {
-            // sender_name: name,
-            // sender_email: email,
-            // sender_message: message
-          // }
-        // }
-      // ]
-    // });
+// try {
+// await mailjet.post('send', { version: 'v3.1' }).request({
+// Messages: [
+// {
+// From: {
+// Email: 'mfz.bin@gmail.com',
+// Name: 'Portfolio Contact'
+// },
+// To: [
+// {
+// Email: 'mfz.bin@gmail.com',
+// Name: 'You'
+// }
+// ],
+// TemplateID: 7402715,
+// TemplateLanguage: true,
+// Variables: {
+// sender_name: name,
+// sender_email: email,
+// sender_message: message
+// }
+// }
+// ]
+// });
 
-    // return new Response(JSON.stringify({ success: true }), { status: 200 });
-  // } catch (err) {
-    // console.error('Mailjet error:', err);
-    // return new Response(JSON.stringify({ success: false }), { status: 500 });
-  // }
+// return new Response(JSON.stringify({ success: true }), { status: 200 });
+// } catch (err) {
+// console.error('Mailjet error:', err);
+// return new Response(JSON.stringify({ success: false }), { status: 500 });
+// }
 // };
 
 
@@ -42,36 +42,36 @@
 import Mailjet from 'node-mailjet'
 
 export default async (req: Request) => {
-  if (req.method !== 'POST')
-    return new Response('Method Not Allowed', { status: 405 })
+    if (req.method !== 'POST')
+        return new Response('Method Not Allowed', { status: 405 })
 
-  const { name, email, message } = await req.json()
-  const mailjet = Mailjet.apiConnect(
-    process.env.MJ_APIKEY_PUBLIC!,
-    process.env.MJ_APIKEY_PRIVATE!
-  )
+    const { name, email, message } = await req.json()
+    const mailjet = Mailjet.apiConnect(
+        process.env.MJ_APIKEY_PUBLIC!,
+        process.env.MJ_APIKEY_PRIVATE!
+    )
 
-  try {
-    await mailjet.post('send', { version: 'v3.1' }).request({
-      Messages: [
-        {
-          From: { Email: 'mfz.bin@gmail.com', Name: 'Portfolio Contact' },
-          To: [{ Email: 'mfz.bin@gmail.com', Name: 'You' }],
-          TemplateID: 7402715,
-          TemplateLanguage: true,
-          Variables: {
-            sender_name: name,
-            sender_email: email,
-            sender_message: message
-          }
-        }
-      ]
-    })
+    try {
+        await mailjet.post('send', { version: 'v3.1' }).request({
+            Messages: [
+                {
+                    From: { Email: 'mfz.bin@gmail.com', Name: 'Portfolio Contact' },
+                    To: [{ Email: 'mfz.bin@gmail.com', Name: 'You' }],
+                    TemplateID: 7402715,
+                    TemplateLanguage: true,
+                    Variables: {
+                        sender_name: name,
+                        sender_email: email,
+                        sender_message: message
+                    }
+                }
+            ]
+        })
 
-    return new Response(JSON.stringify({ success: true }), { status: 200 })
-  } catch (err) {
-    console.error('Mailjet error:', err)
-    return new Response(JSON.stringify({ success: false }), { status: 500 })
-  }
+        return new Response(JSON.stringify({ success: true }), { status: 200 })
+    } catch (err) {
+        console.error('Mailjet error:', err)
+        return new Response(JSON.stringify({ success: false }), { status: 500 })
+    }
 }
 
