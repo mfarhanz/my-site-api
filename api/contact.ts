94d7edbcc095daa4ee2cb1ceb56af4c22374f481
mfarhan <mfarhan@mun.ca>
2025-10-16 21:37:21 -0400

    remove commented code

--- api/contact.ts
+++ api/contact.ts
@@ -46,50 +46,6 @@ const allowedOrigins = [
   'https://mfarhanz.github.io'
 ]
 
-// export default async function handler(req: VercelRequest, res: VercelResponse) {
-//   if (req.method === 'GET') {
-//     res.status(200).send(
-//       'This is the contact API. Please send a POST request with JSON.'
-//     )
-//     return
-//   }
-
-//   if (req.method !== 'POST') {
-//     res.status(405).send('Method Not Allowed')
-//     return
-//   }
-
-//   try {
-//     const { name, email, message } = req.body
-
-//     const mailjet = Mailjet.apiConnect(
-//       process.env.MJ_APIKEY_PUBLIC!,
-//       process.env.MJ_APIKEY_PRIVATE!
-//     )
-
-//     await mailjet.post('send', { version: 'v3.1' }).request({
-//       Messages: [
-//         {
-//           From: { Email: 'mfz.bin@gmail.com', Name: 'Portfolio Contact' },
-//           To: [{ Email: 'mfz.bin@gmail.com', Name: 'You' }],
-//           TemplateID: 7402715,
-//           TemplateLanguage: true,
-//           Variables: {
-//             sender_name: name,
-//             sender_email: email,
-//             sender_message: message
-//           }
-//         }
-//       ]
-//     })
-
-//     res.status(200).json({ success: true })
-//   } catch (err) {
-//     console.error('Mailjet error:', err)
-//     res.status(500).json({ success: false })
-//   }
-// }
-
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const origin = req.headers.origin;
 
