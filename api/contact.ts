74ce63bf644a2c17fbb526f5410b010fa0c601f3
mfarhan <mfarhan@mun.ca>
2025-10-16 21:13:06 -0400

    add my-site URIs to cors exclusion

--- api/contact.ts
+++ api/contact.ts
@@ -2,65 +2,127 @@
 // import { env } from '$env/dynamic/private';
 
 // export const POST = async ({ request }) => {
-  // const { name, email, message } = await request.json();
-  // const mailjet = Mailjet.apiConnect(env.MJ_APIKEY_PUBLIC, env.MJ_APIKEY_PRIVATE);
+// const { name, email, message } = await request.json();
+// const mailjet = Mailjet.apiConnect(env.MJ_APIKEY_PUBLIC, env.MJ_APIKEY_PRIVATE);
 
-  // try {
-    // await mailjet.post('send', { version: 'v3.1' }).request({
-      // Messages: [
-        // {
-          // From: {
-            // Email: 'mfz.bin@gmail.com',
-            // Name: 'Portfolio Contact'
-          // },
-          // To: [
-            // {
-              // Email: 'mfz.bin@gmail.com',
-              // Name: 'You'
-            // }
-          // ],
-          // TemplateID: 7402715,
-          // TemplateLanguage: true,
-          // Variables: {
-            // sender_name: name,
-            // sender_email: email,
-            // sender_message: message
-          // }
-        // }
-      // ]
-    // });
+// try {
+// await mailjet.post('send', { version: 'v3.1' }).request({
+// Messages: [
+// {
+// From: {
+// Email: 'mfz.bin@gmail.com',
+// Name: 'Portfolio Contact'
+// },
+// To: [
+// {
+// Email: 'mfz.bin@gmail.com',
+// Name: 'You'
+// }
+// ],
+// TemplateID: 7402715,
+// TemplateLanguage: true,
+// Variables: {
+// sender_name: name,
+// sender_email: email,
+// sender_message: message
+// }
+// }
+// ]
+// });
 
-    // return new Response(JSON.stringify({ success: true }), { status: 200 });
-  // } catch (err) {
-    // console.error('Mailjet error:', err);
-    // return new Response(JSON.stringify({ success: false }), { status: 500 });
-  // }
+// return new Response(JSON.stringify({ success: true }), { status: 200 });
+// } catch (err) {
+// console.error('Mailjet error:', err);
+// return new Response(JSON.stringify({ success: false }), { status: 500 });
+// }
 // };
 
 
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import Mailjet from 'node-mailjet'
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://mfarhanz.github.io'
+]
+
+// export default async function handler(req: VercelRequest, res: VercelResponse) {
+//   if (req.method === 'GET') {
+//     res.status(200).send(
+//       'This is the contact API. Please send a POST request with JSON.'
+//     )
+//     return
+//   }
+
+//   if (req.method !== 'POST') {
+//     res.status(405).send('Method Not Allowed')
+//     return
+//   }
+
+//   try {
+//     const { name, email, message } = req.body
+
+//     const mailjet = Mailjet.apiConnect(
+//       process.env.MJ_APIKEY_PUBLIC!,
+//       process.env.MJ_APIKEY_PRIVATE!
+//     )
+
+//     await mailjet.post('send', { version: 'v3.1' }).request({
+//       Messages: [
+//         {
+//           From: { Email: 'mfz.bin@gmail.com', Name: 'Portfolio Contact' },
+//           To: [{ Email: 'mfz.bin@gmail.com', Name: 'You' }],
+//           TemplateID: 7402715,
+//           TemplateLanguage: true,
+//           Variables: {
+//             sender_name: name,
+//             sender_email: email,
+//             sender_message: message
+//           }
+//         }
+//       ]
+//     })
+
+//     res.status(200).json({ success: true })
+//   } catch (err) {
+//     console.error('Mailjet error:', err)
+//     res.status(500).json({ success: false })
+//   }
+// }
+
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const origin = req.headers.origin;
+
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method === 'GET') {
-    res.status(200).send(
-      'This is the contact API. Please send a POST request with JSON.'
-    )
-    return
+    res.status(200).send('This is the contact API. Please send a POST request with JSON.');
+    return;
   }
 
   if (req.method !== 'POST') {
-    res.status(405).send('Method Not Allowed')
-    return
+    res.status(405).send('Method Not Allowed');
+    return;
   }
 
   try {
-    const { name, email, message } = req.body
+    const { name, email, message } = req.body;
 
     const mailjet = Mailjet.apiConnect(
       process.env.MJ_APIKEY_PUBLIC!,
       process.env.MJ_APIKEY_PRIVATE!
-    )
+    );
 
     await mailjet.post('send', { version: 'v3.1' }).request({
       Messages: [
@@ -76,11 +138,11 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
           }
         }
       ]
-    })
+    });
 
-    res.status(200).json({ success: true })
+    res.status(200).json({ success: true });
   } catch (err) {
-    console.error('Mailjet error:', err)
-    res.status(500).json({ success: false })
+    console.error('Mailjet error:', err);
+    res.status(500).json({ success: false });
   }
 }
