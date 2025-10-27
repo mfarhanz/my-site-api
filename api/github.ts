84cf8a19734fddafee9dd0057bfd2676a44bbc13
mfarhan <mfarhan@mun.ca>
2025-10-26 22:57:35 -0400

    fix ES related import error

--- api/github.ts
+++ api/github.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { handleCors } from './utils/cors'
+import { handleCors } from './utils/cors.js'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (handleCors(req, res, ['GET'])) return
