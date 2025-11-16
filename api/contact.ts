37c1b3e0fc0c0b21e664fe3543d4299e5bda5532
mfarhan <mfarhan@mun.ca>
2025-11-15 22:28:58 -0500

    added nonce csrf verification for contact endpoint

--- api/contact.ts
+++ api/contact.ts
@@ -2,6 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import Mailjet from 'node-mailjet'
 import { handleCors } from '../utils/cors.js'
 import { CATCH_ALL_ADDRESS } from '../utils/constants.js'
+import { verifyNonce } from '../utils/verify-key.js'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (handleCors(req, res, ['POST', 'GET'])) return;
@@ -13,12 +14,16 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   try {
     // hp - honeypot field in client
-    const { name, email, message, hp } = req.body;
+    const { name, email, message, hp, nonce } = req.body;
 
-    console.log(name, email, message, hp);
     // honeypot check — if anything is filled in, it's a bot
     if (hp && hp.trim() !== '') {
       return res.status(418).json({ success: false, error: "Bot detected!" });
+    }
+
+    // check if request is coming from the contacts/ page of my site, by checking the provided nonce
+    if (!verifyNonce(nonce)) {
+      return res.status(401).json({ success: false, error: "Invalid/missing nonce!" });
     }
 
     const mailjet = Mailjet.apiConnect(
---
+++ utils/verify-key.ts
@@ -0,0 +1,20 @@
+import crypto from 'crypto';
+
+export function verifyNonce(nonce: string) {
+	if (!nonce) return false;
+
+	const [payload, signature] = nonce.split('.');
+	if (!payload || !signature) return false;
+
+	const expectedSig = crypto
+		.createHmac('sha256', process.env.NONCE_SECRET!)
+		.update(payload)
+		.digest('hex');
+
+	// using timingSafeEqual to avoid timing attacks
+	const valid =
+		signature.length === expectedSig.length &&
+		crypto.timingSafeEqual(Buffer.from(signature, 'hex'), Buffer.from(expectedSig, 'hex'));
+
+	return valid;
+}
